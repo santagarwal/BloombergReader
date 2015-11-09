fecb9fae9482f8e4d142dbc569352c87df840585
Andrii Tkach <Andrii Tkach>
2015-11-09 20:57:47 +0100

    simple MVVM template

---
+++ BloombergReader/Model/Product.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace BloombergReader.Model
+{
+    public class Product : INotifyPropertyChanged
+    {
+        private int m_ID;
+        private string m_Name;
+        private double m_Price;
+
+        public int ID
+        {
+            get
+            {
+                return m_ID;
+            }
+            set
+            {
+                m_ID = value;
+                OnPropertyChanged("ID");
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return m_Name;
+            }
+            set
+            {
+                m_Name = value;
+                OnPropertyChanged("Name");
+            }
+
+        }
+
+        public double Price
+        {
+
+            get
+            {
+                return m_Price;
+            }
+            set
+            {
+                m_Price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
--- BloombergReader/View/MainWindow.xaml.cs
+++ BloombergReader/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BloombergReader
+namespace BloombergReader.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
