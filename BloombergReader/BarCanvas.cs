2f2effb47bd2b0103c86340484cc61e5e63d5ce7
and85 <andreytkach85@gmai.com>
2015-12-24 16:00:25 +0100

    Draw coordinate system

--- BloombergReader/BarCanvas.cs
+++ BloombergReader/BarCanvas.cs
@@ -6,33 +6,22 @@ using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows;
 using System.Windows.Controls;
-
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
-using BloombergReader.Model;
 
+using BloombergReader.Model;
 using BloombergReader.ViewModel;
+using BloombergReader.View.Shapes;
+using System.Globalization;
 
 namespace BloombergReader
 {
-    // http://www.anotherearlymorning.com/example/Makotoro/DynamicCanvas.html
-    // http://www.codeproject.com/Articles/139216/A-Simple-Technique-for-Data-binding-to-the-Positio
-
-    //http://techbus.safaribooksonline.com/book/programming/microsoft-wpf/9781849686228/1dot-foundations/id286723836?query=((Dependency+property))#snippet
-
     public class BarCanvas : Canvas
     {
-        /*
-        public static readonly DependencyProperty IsSpinningProperty = DependencyProperty.Register("IsSpinning", 
-            typeof(Boolean), typeof(Boolean));
-
-        public bool IsSpinning
-        {
-            get { return (bool)GetValue(IsSpinningProperty); }
-            set { SetValue(IsSpinningProperty, value); }
-        }
-         */
-
+        private const double AxisThickness = 1;
+        private const double XShiftFromScreenBorder = 50;
+        private const double YShiftFromScreenBorder = 50;
+        private const double AxisFontSize = 16;
 
         public static readonly DependencyProperty BarsProperty = DependencyProperty.Register("Bars",
             typeof(ObservableCollection<Bar>), typeof(BarCanvas), new FrameworkPropertyMetadata(null, OnBarsChanged));
@@ -46,32 +35,64 @@ namespace BloombergReader
         private static void OnBarsChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
         }
-
-        public BarCanvas()
-        {
-            /*
-            foreach (var item in Bars)
-            {
-                MessageBox.Show(item.Open.ToString());
-            }
-             */
-        }
-
+        
         protected override void OnRender(DrawingContext dc)
         {
+            DrawСoordinateSystem(dc);
+
+            // draw all bars
             for (int i=0; i<Bars.Count; i++)
             {
                 Bar bar = Bars[i];
 
-                SolidColorBrush mySolidColorBrush = new SolidColorBrush();
-                mySolidColorBrush.Color = Colors.LimeGreen;
-                Pen myPen = new Pen(Brushes.Blue, 1);
-
-                Point p1 = new Point(i * 10 + 10, (double)bar.Open);
-                Point p2 = new Point(i * 10 + 10, (double)bar.Close);
-
-                dc.DrawLine(myPen, p1, p2);
+                // DrawBar(dc, i, 10, 10, bar);
             }
+        }
+
+        private void DrawСoordinateSystem(DrawingContext dc)
+        {
+            SolidColorBrush solidColorBrush = new SolidColorBrush();
+            solidColorBrush.Color = Colors.Black;
+            Pen pen = new Pen(solidColorBrush, AxisThickness);
+            pen.DashStyle = DashStyles.Solid;
+    
+            FormattedText priceText = new FormattedText(
+                "Price",
+                CultureInfo.InvariantCulture,
+                FlowDirection.LeftToRight,
+                new Typeface("Verdana"),
+                AxisFontSize,
+                solidColorBrush);
+
+            ArrowLine xAsix = DefineXAxis();
+            ArrowLine yAsix = DefineYAxis();
+
+            dc.DrawGeometry(solidColorBrush, pen, xAsix.DefiningGeometry);
+            dc.DrawGeometry(solidColorBrush, pen, yAsix.DefiningGeometry);
+
+            dc.DrawText(priceText, new Point(yAsix.X2 - priceText.Width / 2, yAsix.Y2 - priceText.Height));
+        }
+
+        private ArrowLine DefineYAxis()
+        {
+            ArrowLine yAsix = new ArrowLine();
+            yAsix.ArrowEnds = ArrowEnds.End;
+            yAsix.X1 = XShiftFromScreenBorder;
+            yAsix.Y1 = ActualHeight - YShiftFromScreenBorder;
+            yAsix.X2 = yAsix.X1;
+            yAsix.Y2 = YShiftFromScreenBorder;
+            return yAsix;
+        }
+
+        private ArrowLine DefineXAxis()
+        {
+            ArrowLine xAsix = new ArrowLine();
+            xAsix.ArrowEnds = ArrowEnds.End;
+            xAsix.X1 = XShiftFromScreenBorder;
+            xAsix.Y1 = ActualHeight - YShiftFromScreenBorder;
+            xAsix.X2 = ActualWidth - XShiftFromScreenBorder;
+            xAsix.Y2 = xAsix.Y1;
+            return xAsix;
         }
     }
 }
