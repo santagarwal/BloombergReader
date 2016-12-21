944efe1ad97c3c10f75cb2e216193575b3ab4f07
and85 <andreytkach85@gmai.com>
2016-12-21 20:10:10 +0100

    Added a table that shows a dummy list of bars

---
+++ BloombergReaderWeb/Controllers/IntradayBarTableController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BloombergReaderWeb.Models;
+
+namespace BloombergReaderWeb.Controllers
+{
+    public class IntradayBarTableController : Controller
+    {
+        // GET: IntradayBarTable
+        public ActionResult Index()
+        {
+            var bars = new List<Bar>
+            {
+                new Bar()
+                {
+                    Date = DateTime.Now,
+                    Open = 12.3m,
+                    Close = 11.3m,
+                    High = 15.4m,
+                    Low = 3.2m,
+                    Volume = 15
+                },
+                new Bar()
+                {
+                    Date = DateTime.Now,
+                    Open = 15.3m,
+                    Close = 13.3m,
+                    High = 16.4m,
+                    Low = 3.1m,
+                    Volume = 18
+                }
+            };
+
+            return View(bars);
+        }
+    }
+}
---
+++ BloombergReaderWeb/Models/Bar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BloombergReaderWeb.Models
+{
+    public class Bar
+    {
+        public DateTime Date { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public int Volume { get; set; }
+
+        public override string ToString()
+        {
+            return $"Date: {Date}; Open: {Open}; High: {High}; Low: {Low}; Close: {Close}; Volume: {Volume}";
+        }
+    }
+}
